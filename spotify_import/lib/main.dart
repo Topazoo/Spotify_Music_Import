b6abdb7555bb2c646ea470d856fb57b873d8adfb
Peter Swanson <topazoo42@gmail.com>
2018-12-04 23:38:52 -0800

    Started theme library

--- spotify_import/lib/main.dart
+++ spotify_import/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'theme.dart' as AppThemes;
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+  
     return MaterialApp(
       title: 'Spotify Import',
       theme: ThemeData(
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: AppThemes.MainThemeSwatch.swatch,
       ),
       home: MyHomePage(title: 'Import Music to Spotify'),
     );
---
+++ spotify_import/lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Main Theme Color Swatch
+class MainThemeSwatch {
+  static MaterialColor swatch = const MaterialColor(0xFF1db954,
+      const {
+        50 : const Color(0xFFffffff),
+        100 : const Color(0xFFd3f8e0),
+        200 : const Color(0xFFa7f1c1),
+        300 : const Color(0xFF7beaa2),
+        400 : const Color(0xFF4fe383),
+        500 : const Color(0xFF38e073),
+        600 : const Color(0xFF22dd64),
+        700 : const Color(0xFF1cb050),
+        800 : const Color(0xFF15843c),
+        900 : const Color(0xFF0e5828),
+      }
+    );
+}
